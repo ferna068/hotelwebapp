09d313be397cbbd1000f8a1f595d202fd3481a33
Emanuel Fernandez <ferna068@gmail.com>
2024-11-01 21:11:13 -0500

    Components and Bootstrap 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'contactus',
+        component: ContactusComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    }
+];
